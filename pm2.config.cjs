c626885c17cdaea594bcb2c727dc9f56ee524899
FourKress <511970747@qq.com>
2024-03-05 11:44:36 +0800

    feat: init

---
+++ pm2.config.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      env: {
+        NODE_ENV: 'dev'
+      },
+      env_production: {
+        NODE_ENV: 'prod'
+      },
+      name: process.env.MALL_APP_NAME,
+      port: '8888',
+      script: './.output/server/index.mjs',
+      watch: false,
+      merge_logs: true,
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      min_uptime: '60s',
+      max_restarts: 30,
+      autorestart: true,
+      restart_delay: 60,
+      exec_mode: 'cluster',
+      instances: 'max'
+    }
+  ]
+}
--- server.sh
+++ server.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
+app_name="ruilin-mall"
+
 echo '---开始执行git checkout main---'
-git checkout main
+#git checkout main
 echo '---开始执行git pull---'
-git pull
+#git pull
 echo '---git pull执行完毕，pnpm install---'
-pnpm i
+#pnpm i
 echo '---pnpm install执行完毕，开始执行pnpm build---'
-pnpm build
-echo '---开始执行pnpm build执行完毕 开始执行pnpm pm2---'
-pnpm pm2:prod
-echo '---pnpm pm2执行完毕 服务启动成功---'
+#pnpm build
+echo '---开始执行pnpm build执行完毕 开始检查应用运行状态'
+if pm2 ls | grep -q $app_name; then
+  echo "---应用已经启动，开始检查应用运行状态---"
+  app_status=$(pm2 show $app_name --no-color | grep "status" | awk '{print $4}')
+  if [[ "$app_status" == *"online"* ]]; then
+    echo "---应用正在运行中，执行reload重新加载---"
+    MALL_APP_NAME=$app_name pm2 reload pm2.config.cjs --env prod
+  elif [[ "$app_status" == *"stopped"* ]]; then
+    echo "---应用停止运行，执行restart重新启动---"
+    MALL_APP_NAME=$app_name pm2 restart pm2.config.cjs --env prod
+  fi
+else
+  echo "---应用没有启动，执行start启动---"
+  MALL_APP_NAME=$app_name pm2 start pm2.config.cjs --env prod
+fi
+
+echo '---脚本执行完毕 服务启动成功---'
