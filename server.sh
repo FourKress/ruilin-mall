59515f6059dd35ff0edcffc7df207521e3978494
FourKress <511970747@qq.com>
2024-03-19 21:23:23 +0800

    feat: init

--- server.sh
+++ server.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-app_name="ruilin-mall"
+branch=""
 
-echo '---开始执行git checkout main---'
-git checkout main
+if [ "$1" == "test" ]
+then
+  branch="test"
+elif [ "$1" == "prod" ]
+then
+  branch="main"
+else
+  echo '---参数错误，执行失败---'
+  exit 1
+fi
+
+app_name="$1-ruilin-mall"
+
+echo "---开始执行git checkout $branch---"
+git checkout $branch
 echo '---开始执行git pull---'
 git pull
 echo '---git pull执行完毕，pnpm install---'
@@ -16,14 +29,14 @@ if pm2 ls | grep -q $app_name; then
   app_status=$(pm2 show $app_name --no-color | grep "status" | awk '{print $4}')
   if [[ "$app_status" == *"online"* ]]; then
     echo "---应用正在运行中，执行reload重新加载---"
-    MALL_APP_NAME=$app_name pm2 reload pm2.config.cjs --env production
+    MALL_APP_NAME=$app_name pm2 reload pm2.config.cjs --env $1
   elif [[ "$app_status" == *"stopped"* ]]; then
     echo "---应用停止运行，执行restart重新启动---"
-    MALL_APP_NAME=$app_name pm2 restart pm2.config.cjs --env production
+    MALL_APP_NAME=$app_name pm2 restart pm2.config.cjs --env $1
   fi
 else
   echo "---应用没有启动，执行start启动---"
-  MALL_APP_NAME=$app_name pm2 start pm2.config.cjs --env production
+  MALL_APP_NAME=$app_name pm2 start pm2.config.cjs --env $1
 fi
 
 echo '---脚本执行完毕 服务启动成功---'
