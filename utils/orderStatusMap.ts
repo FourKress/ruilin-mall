5d5cd7b0c67868c7ec8f07651ef758c6bb1ca41b
FourKress <four983664@gmail.com>
2024-10-31 22:26:05 +0800

    fix: bug修复

--- utils/orderStatusMap.ts
+++ utils/orderStatusMap.ts
@@ -1,13 +1,11 @@
 const orderStatusTipsMap = {
   '-1': 'Transaction closed', // 取消订单
   0: 'Unpaid...', // 待支付
-  1: 'Under Review...', // 待审核
   2: '  Shipment', //待发货
   3: 'Shipping...', // 运输中
   4: 'Awaiting Receipt...', //待收货
   5: 'Transaction completed', // 订单完结
   6: 'Awaiting Refund', // 退款中
-  7: 'Payment Confirm...', // 支付确认中
   8: 'After-sales Support...' // 退款审核
 }
 
